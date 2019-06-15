81516342d978ac8c41f24cfcc2b4189e5a00245e
Su Guangjun <guangjun@Sus-MacBook-Pro.local>
2019-06-14 20:25:44 -0700

    add comments for schedulor function

--- src/models/schedulor.py
+++ src/models/schedulor.py
@@ -13,6 +13,15 @@ import itertools
 
 
 def shift_assignment(data_file, staff_type, shift_scenario):
+    """
+    Optimization model for shift assignment.
+
+    :param data_file: path to input file
+    :param staff_type: staff type, i.e. 'circulator', 'scrub'
+    :param shift_scenario: scenario for shift assignment, i.e. Shift_SC1, Shift_SC2, ... (align with the sheet name in
+                           input file.)
+    :return: shift assignment solution
+    """
     # TODO: maximum number of available nurses by skill
     # TODO: maximum number of nurse working hours groupby week (FTE)
 
@@ -27,6 +36,7 @@ def shift_assignment(data_file, staff_type, shift_scenario):
     ##  DECLARE DECISION VARIABLES  ##
     ##################################
     skd_tuples = tuple(itertools.product(params['P1_s'], params['P4_k'], params['P6_d']))
+    # X_skd is the decision variable => # of nurses for shift d with skill k on day d
     X_skd = m.addVars(skd_tuples, vtype=GRB.INTEGER, name='shift_assignment')
 
     ############################
@@ -34,24 +44,30 @@ def shift_assignment(data_file, staff_type, shift_scenario):
     ############################
 
     # Neuro Demand
+    # Supply of Neuro nurses should be greater than or equal to Neuro demand
     m.addConstrs(((quicksum([X_skd[(s, k, d)] * params['P3_st'][(s, t)] for s in params['P1_s'] for k in ['Base_Ortho_Neuro']]) >=
                   quicksum([params['P10_ktd'][(k, t, d)] for k in ['Base_Ortho_Neuro']]))
                   for t in params['P2_t'] for d in params['P6_d']),
                  name='neuro_demand')
 
     # Ortho Demand
+    # Supply of (Ortho and Neuro) should be greater than or equal to (Neuro and Ortho) demand
+    # Nurse with Neuro skill can perform Ortho procedures
     m.addConstrs(((quicksum([X_skd[(s, k, d)] * params['P3_st'][(s, t)] for s in params['P1_s'] for k in ['Base_Ortho_Neuro', 'Base_Ortho']]) >=
                   quicksum([params['P10_ktd'][(k, t, d)] for k in ['Base_Ortho_Neuro', 'Base_Ortho']]))
                   for t in params['P2_t'] for d in params['P6_d']),
                  name='ortho_demand')
 
     # Transplant Demand
+    # Supply of Transplant should be greater than or equal to Transplant demand
     m.addConstrs(((quicksum([X_skd[(s, k, d)] * params['P3_st'][(s, t)] for s in params['P1_s'] for k in ['Base_Transplant']]) >=
                   quicksum([params['P10_ktd'][(k, t, d)] for k in ['Base_Transplant']]))
                   for t in params['P2_t'] for d in params['P6_d']),
                  name='transplant_demand')
 
     # Base Demand
+    # Supply of (Baes, Neuro, Ortho, Transplant) should be greater than or equal to (Baes, Neuro, Ortho, Transplant) demand
+    # Nurse with (Baes, Neuro, Ortho, Transplant) can perform Base procedures
     m.addConstrs(((quicksum([X_skd[(s, k, d)] * params['P3_st'][(s, t)] for s in params['P1_s'] for k in ['Base_Ortho_Neuro', 'Base_Ortho', 'Base_Transplant', 'Base']]) >=
                   quicksum([params['P10_ktd'][(k, t, d)] for k in ['Base_Ortho_Neuro', 'Base_Ortho', 'Base_Transplant', 'Base']]))
                   for t in params['P2_t'] for d in params['P6_d']),
@@ -60,10 +76,13 @@ def shift_assignment(data_file, staff_type, shift_scenario):
     #####################
     ##  SET OBJECTIVE  ##
     #####################
+    # objective of this problem is the total cost of staffing nurses
+    # now, we assume the hourly cost is different from skillset, refer to 'config.py'
     m.setObjective(quicksum([X_skd[(s, k, d)] * params['P5_sk'][(s, k)] for s in params['P1_s'] for k in params['P4_k'] for d in params['P6_d']]))
 
     m.optimize()
 
+    # print out the overall solution of shift assignment
     for d in params['P6_d']:
         print('========================')
         print(f'Scheduling {d}...')
